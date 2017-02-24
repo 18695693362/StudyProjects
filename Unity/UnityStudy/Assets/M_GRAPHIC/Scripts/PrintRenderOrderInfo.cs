e5fdb02e07eac4bb8bbd234fd26d90c9ea8c4542
wolfand11 <smile_guodong@163.com>
2017-02-25 00:12:12 +0800

    improve Unity Study project

---
+++ Unity/UnityStudy/Assets/M_GRAPHIC/Scripts/PrintRenderOrderInfo.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+
+public class PrintRenderOrderInfo : MonoBehaviour {
+
+    public bool isUpdateRenderQueue = false;
+    public int renderQueue = 2000;
+    void OnEnable()
+    {
+        PrintOrderInfo(GetRenderer());
+    }
+
+    Renderer GetRenderer()
+    {
+        Renderer renderer = null;
+        if (renderer == null)
+            renderer = gameObject.GetComponent<MeshRenderer>();
+        if (renderer == null)
+            renderer = gameObject.GetComponent<MeshRenderer>();
+        if (renderer == null)
+            renderer = gameObject.GetComponent<SpriteRenderer>();
+        return renderer;
+    }
+
+    void PrintOrderInfo(Renderer renderer)
+    {
+        if (renderer != null)
+        {
+            StringBuilder log = new StringBuilder();
+            log.Append("--- Render OrderInfo --- " + name + "\n");
+            log.Append(" sortingLayer = " + renderer.sortingLayerName + "\n");
+            log.Append(" sortingOrder = " + renderer.sortingOrder + "\n");
+            if(renderer.material != null)
+            {
+                log.Append(" renderQueue = " + renderer.material.renderQueue);
+            }
+            Debug.Log(log);
+        }
+    }
+
+    void Update()
+    {
+        Renderer renderer = GetRenderer();
+        if(isUpdateRenderQueue && renderer != null && renderer.material != null)
+        {
+            if(renderer.material.renderQueue!=renderQueue)
+            {
+                renderer.material.renderQueue = renderQueue;
+            }
+        }
+    }
+}
