2584f9965599354b21f0831c78abe8996b475ead
wolfand11 <smile_guodong@163.com>
2017-03-10 00:09:08 +0800

    add GetComponent test

---
+++ Unity/UnityStudy/Assets/M_SCRIPT/Scripts/S5_GetComponent.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class S5_GetComponent : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+        var obj = new GameObject();
+
+        // 下面两种效果是一样的
+        foreach ( var t in gameObject.GetComponents ( typeof ( Component ) ) )
+        //foreach ( var t in this.GetComponents<Component> ( ) )
+        {
+            Debug.Log ( t.ToString ( ) );
+        }
+
+        var widget = GetComponent<UIWidget>();
+        if ( widget != null )
+        {
+            Debug.Log ( "从UIImage上获取UIWidget成功 --- " + widget.ToString ( ) );
+        }
+        else
+        {
+            Debug.Log ( "从UIImage上获取UIWidget失败 " );
+        }
+
+        if (obj.transform != null)
+        {
+            Debug.Log ( "new出来的GameObject 会 自动附带Transform" );
+        }
+        else
+        {
+            Debug.Log ( "new出来的GameObject 不会 自动附带Transform" );
+        }
+	}
+}
