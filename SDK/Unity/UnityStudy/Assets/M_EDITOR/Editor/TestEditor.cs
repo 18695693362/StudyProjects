7fb824464f2e6d70dd79eb9e9539e9e9467da2c6
guodong <smile_guodong@163.com>
2018-03-27 10:40:00 +0800

    improve Unity Study

---
+++ SDK/Unity/UnityStudy/Assets/M_EDITOR/Editor/TestEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TestEditor : MonoBehaviour {
+
+    void Test()
+    {
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
