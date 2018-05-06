2f2d692554977816cb4427b9fec92f178b6940c6
guodong <smile_guodong@163.com>
2018-05-06 20:30:59 +0800

    add Mesh test

---
+++ SDK/Unity/UnityStudy/Assets/M_MISC/Scripts/S01_MeshVert.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S01_MeshVert : MonoBehaviour
+{
+    void Update()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        if (mesh == null) return;
+
+        Vector3[] vertices = mesh.vertices;
+        Vector3[] normals = mesh.normals;
+        int i = 0;
+        while (i < vertices.Length)
+        {
+            vertices[i] += normals[i] * Mathf.Sin(Time.time) * 0.1f;
+            i++;
+        }
+        mesh.vertices = vertices;
+    }
+}
